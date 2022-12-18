cd7a5243ebd0feb8478d9f1932961ed430adf73d
digitalbreadllc <digitalbreadllc@gmail.com>
2022-12-18 11:03:22 -0700

    work

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OdoMec.java
@@ -0,0 +1,127 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@TeleOp(name="OdoMec")
+
+
+public class OdoMec extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    //Motors
+    //Drivetrain
+    private DcMotor rightFront; //front right 0
+    private DcMotor leftFront; //front left 2
+    private DcMotor rightRear; //rear right 1
+    private DcMotor leftRear; //rear left 3
+
+    //Encoders
+    private DcMotor verticalRight; // 1
+    private DcMotor verticalLeft; // 3
+    private DcMotor horizontal; // 0
+
+    BNO055IMU imu;                // Additional Gyro device
+    Orientation angles;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables
+        //Motors
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+        leftRear = hardwareMap.get(DcMotor.class, "leftRear");
+
+        //Encoders
+        verticalRight = hardwareMap.get(DcMotor.class, "rightRear");
+        verticalLeft = hardwareMap.get(DcMotor.class, "leftRear");
+        horizontal = hardwareMap.get(DcMotor.class, "rightFront");
+
+        //Set motor directions
+        /////////////////////////////////////////////////////////////////////////////////
+        //rightFront.setDirection(DcMotor.Direction.FORWARD);
+        //leftFront.setDirection(DcMotor.Direction.REVERSE);
+        //rightRear.setDirection(DcMotor.Direction.FORWARD);
+        //leftRear.setDirection(DcMotor.Direction.REVERSE);
+
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightRear.setDirection(DcMotor.Direction.REVERSE);
+        leftRear.setDirection(DcMotor.Direction.FORWARD);
+
+
+        //Set motor modes
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        verticalLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        horizontal.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        telemetry.addData("Status", "OdoMec2 is ready to run!");
+        telemetry.update();
+
+        //Wait for press play
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //Drivetrain
+            double forward = gamepad1.left_stick_y;
+            double strafe = -gamepad1.left_stick_x;
+            double turn = -gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(forward)+Math.abs(strafe)+Math.abs(turn), 1);
+
+            double rightFrontPower = (forward - strafe - turn) / denominator;
+            double leftFrontPower = (forward + strafe + turn) / denominator;
+            double rightRearPower = (forward + strafe - turn) / denominator;
+            double leftRearPower = (forward - strafe + turn) / denominator;
+
+            if (gamepad1.left_bumper) {
+                rightFrontPower = Range.clip(rightFrontPower, -0.4, 0.4);
+                leftFrontPower = Range.clip(leftFrontPower, -0.4, 0.4);
+                rightRearPower = Range.clip(rightRearPower, -0.4, 0.4);
+                leftRearPower = Range.clip(leftRearPower, -0.4, 0.4);
+            } else {
+                rightFrontPower = Range.clip(rightFrontPower, -0.8, 0.8);
+                leftFrontPower = Range.clip(leftFrontPower, -0.8, 0.8);
+                rightRearPower = Range.clip(rightRearPower, -0.8, 0.8);
+                leftRearPower = Range.clip(leftRearPower, -0.8, 0.8);
+            }
+
+
+            rightFront.setPower(rightFrontPower);
+            leftFront.setPower(leftFrontPower);
+            rightRear.setPower(rightRearPower);
+            leftRear.setPower(leftRearPower);
+
+            telemetry.addData("Status", "Run " + runtime.toString());
+            telemetry.addData("Motors", "forward (%.2f), strafe (%.2f),turn (%.2f)" , forward, strafe, turn);
+            telemetry.update();
+        }
+    }
+}
