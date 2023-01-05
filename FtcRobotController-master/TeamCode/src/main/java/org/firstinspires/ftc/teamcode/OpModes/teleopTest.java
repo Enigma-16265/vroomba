9f7f49af834dce5e164762cdba22b85a89f0e714
russmc <digitalbreadllc@gmail.com>
2023-01-05 10:39:57 -0700

    add libraries

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/teleopTest.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/teleopTest.java
@@ -1,24 +1,31 @@
 package org.firstinspires.ftc.teamcode.OpModes;
-
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-
+import java.util.Locale;
 @TeleOp(name = "testSensor")
 
 public class teleopTest extends LinearOpMode {
 
 
-    private DistanceSensor clawDistance;
+    ColorSensor sensorColor;
+    DistanceSensor sensorDistance;
 
     @Override
     public void runOpMode() {
         // you can use this as a regular DistanceSensor.
-        clawDistance = hardwareMap.get(DistanceSensor.class, "clawDistance");
+        sensorColor = hardwareMap.get(ColorSensor.class, "clawDistance");
+
+        // get a reference to the distance sensor that shares the same name.
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "clawDistance");
 
         // you can also cast this to a Rev2mDistanceSensor if you want to use added
         // methods associated with the Rev2mDistanceSensor class.
@@ -26,17 +33,57 @@ public class teleopTest extends LinearOpMode {
         telemetry.addData(">>", "Press start to continue");
         telemetry.update();
 
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F, 0F, 0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // sometimes it helps to multiply the raw RGB values with a scale factor
+        // to amplify/attentuate the measured values.
+        final double SCALE_FACTOR = 255;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
         waitForStart();
         while (opModeIsActive()) {
             // generic DistanceSensor methods.
 
-            telemetry.addData("deviceName", clawDistance.getDeviceName());
-            telemetry.addData("range", String.format("%.01f mm", clawDistance.getDistance(DistanceUnit.MM)));
-            telemetry.addData("range", String.format("%.01f cm", clawDistance.getDistance(DistanceUnit.CM)));
-            telemetry.addData("range", String.format("%.01f m", clawDistance.getDistance(DistanceUnit.METER)));
-            telemetry.addData("range", String.format("%.01f in", clawDistance.getDistance(DistanceUnit.INCH)));
+            Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+                    (int) (sensorColor.green() * SCALE_FACTOR),
+                    (int) (sensorColor.blue() * SCALE_FACTOR),
+                    hsvValues);
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Distance (mm)",
+                    String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.MM)));
+            telemetry.addData("Alpha", sensorColor.alpha());
+            telemetry.addData("Red  ", sensorColor.red());
+            telemetry.addData("Green", sensorColor.green());
+            telemetry.addData("Blue ", sensorColor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+
+            // change the background color to match the color detected by the RGB sensor.
+            // pass a reference to the hue, saturation, and value array as an argument
+            // to the HSVToColor method.
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                }
+            });
 
             telemetry.update();
         }
+
+        // Set the panel back to the default color
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.WHITE);
+            }
+        });
+
     }
 }
